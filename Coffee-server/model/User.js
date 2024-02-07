e19731025cf596af479a346fe4620cb7fe68738d
skilljeminkikani <skilljeminkikani9510@gmail.com>
2024-02-07 10:35:23 +0530

    default Images

--- Coffee-server/model/User.js
+++ Coffee-server/model/User.js
@@ -1,59 +1,60 @@
-const mongoose = require('mongoose')
-const bcrypt = require('bcrypt')
-const validator = require('validator')
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const validator = require("validator");
 
-const UserSchema = new mongoose.Schema({
-
+const UserSchema = new mongoose.Schema(
+  {
     name: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     email: {
-        type: String,
-        required: true,
-        unique: true,
-        validate: [validator.isEmail, "Please provide email"],
+      type: String,
+      required: true,
+      unique: true,
+      validate: [validator.isEmail, "Please provide email"],
     },
     password: {
-        type: String,
-        required: true,
-        minLength: 8,
-        select: false,
-
+      type: String,
+      required: true,
+      minLength: 8,
+      select: false,
     },
     confrimPassword: {
-        type: String,
-        required: true,
-        minLength: 8,
-        select: false,
-        validate: {
-            validator: function (el) {
-                return el === this.password;
-            },
+      type: String,
+      required: true,
+      minLength: 8,
+      select: false,
+      validate: {
+        validator: function (el) {
+          return el === this.password;
         },
-        message: "Password are not match",
+      },
+      message: "Password are not match",
     },
     isVerified: {
-        type: Boolean,
-        default: false,
-        select: false,
+      type: Boolean,
+      default: false,
+      select: false,
     },
     otp: {
-        type: Number,
+      type: Number,
     },
     role: {
-        type: String,
-        enum: ["user", "admin"],
-        default: "user",
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
     },
     profileImage: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
+      default:
+        "https://stock.adobe.com/in/images/default-profile-picture/64672736",
     },
-},
-    {
-        timestamps: true,
-    }
+  },
+  {
+    timestamps: true,
+  }
 );
 
 module.exports = mongoose.model("User", UserSchema);
